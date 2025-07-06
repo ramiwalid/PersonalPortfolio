69e610b0a9d5a7216a1de6aba14c6b93b599a232
ramiwalid <ramiwalid5@gmail.com>
2025-07-05 23:32:53 -0400

    Created components folder for react components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Navbar() {
+	return (
+		<>
+			<button>Home</button>
+			<button>Resume</button>
+			<button>LinkedIn</button>
+			<button>GitHub</button>
+		</>
+	)
+}
+
+export default Navbar;
