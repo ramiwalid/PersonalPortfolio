ccf8d777e408aa7e53706e41480e20b9f30781e1
ramiwalid <ramiwalid5@gmail.com>
2025-07-07 12:42:53 -0400

    Created new directory for skills and related components

---
+++ frontend/src/components/Skills/Skills.jsx
@@ -0,0 +1,13 @@
+
+function Skills() {
+	return (
+		<>
+			<div id='skills-section'>
+				<h1 className='text-white mt-70 font-mono text-4xl'>Skills
+				</h1>	
+			</div>
+		</>
+	)
+}
+
+export default Skills;
