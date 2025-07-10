18cca5fe0dc952311d1fc0fc131a08980294139a
ramiwalid <ramiwalid5@gmail.com>
2025-07-09 23:42:31 -0400

    Fixed certificate outline

--- frontend/src/components/Skills/Certificates.jsx
+++ frontend/src/components/Skills/Certificates.jsx
@@ -4,7 +4,7 @@ export default function Certificates({ name, icon, description, link }) {
 			<div className="flex justify-center mb-5">{icon}</div>	
 			<h1 className="text-2xl">{name}</h1>
 			<p className="text-s p-4">{description}</p>
-			<div className="flex justify-end">
+			<div className="flex w-fit mx-auto border-2 px-3 py-1 border-double">
 				<a href={link} target="_blank">Click to view credential</a>
 			</div>
 		</div>
